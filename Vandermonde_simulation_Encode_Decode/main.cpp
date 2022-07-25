0e252453d0a0ec50f27c3a2a5c9300b6507abefe
Chinabai <Chinabai@gmail.com>
2022-07-25 20:38:39 +0800

    Initial Commit

---
+++ Vandermonde_simulation_Encode_Decode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Vandermonde_simulation_Encode_Decode
+//
+//  Created by 李萍萍 on 2022/7/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
